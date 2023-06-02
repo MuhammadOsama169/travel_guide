18045e4eed5ea01e875c61c60c82dd0dfd48beb9
MuhammadOsama169 <m.osama169@gmail.com>
2023-06-02 19:10:18 +0300

    bug: authOptions is not a valid Route

---
+++ app/api/auth/[...nextauth]/auth-config.ts
@@ -0,0 +1,50 @@
+import { AuthOptions } from "next-auth";
+import GoogleProvider from 'next-auth/providers/google';
+import User from '../../../../models/user';
+import { connectToDB } from '../../../../utils/database';
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "@/lib/prisma";
+
+
+const authOptions: AuthOptions ={
+  // adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    })
+  ],
+  callbacks: {
+    async session({ session }) {
+      // store the user id from MongoDB to session
+      const sessionUser = await User.findOne({ email: session.user.email });
+      session.user.id = sessionUser._id.toString();
+
+      return session;
+    },
+    async signIn({ account, profile, user, credentials }) {
+      try {
+        await connectToDB();
+
+        // check if user already exists
+        const userExists = await User.findOne({ email: profile.email });
+
+        // if not, create a new document and save user in MongoDB
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ", "").toLowerCase(),
+            image: profile.picture,
+          });
+        }
+
+        return true
+      } catch (error) {
+        console.log("Error checking if user exists: ", error.message);
+        return false
+      }
+    },
+  }
+};
+
+export default authOptions;
